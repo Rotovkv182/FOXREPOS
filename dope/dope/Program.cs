9ebaa89a63a52e04bb8d4a4c6f04a986a0c8a666
rotovkv182@outlook.com <rotovkv182@outlook.com>
2020-12-20 20:43:20 +0300

    Exceptions added

--- dope/dope/Program.cs
+++ dope/dope/Program.cs
@@ -26,74 +26,75 @@ namespace dope
          */
         public static void log(string message) //метод для логгирования
         {
-            try
-            {
-                File.AppendAllText("E:\\fox.log", message); //класс и метод для записи в файл
-            }
-            catch (FileNotFoundException)
-            {
-                Console.WriteLine("File not found!");
-            }
+           try
+           { File.AppendAllText("E:\\fox.log", message); /*класс и метод для записи в файл*/ }
+           catch (FileNotFoundException)
+           { Console.WriteLine("File not found!"); }
         }
         static void Main(string[] args)
         {
-            log("\n" + DateTime.Now + "\n");
-            Console.Write("Количество Чисел: ");
-            int N = Convert.ToInt32(Console.ReadLine()); /*количество чисел на входе*/
-            log("Введено кол-во чисел: " + N + '\n');
-            int x; /*исходные данные*/
-            int a2 = 0, b2 = 0; /*макс. числа, кратные 2, но не кратные 5*/
-            int a5 = 0, b5 = 0; /*макс. числа, кратные 5, но не кратные 2*/
-            int a0 = 0, b0 = 0; /*максимальные числа, не кратные 5 и 2*/
-            int R; /*введенное контрольное значение*/
-            int m; /*вычисленное контрольное значение*/
-            for (int i = 1; i <= N; ++i)
+            try
             {
-                Console.Write("Исходные Данные: ");
-                x = Convert.ToInt32(Console.ReadLine());
-                log("Введено значение для: Исходные Данные " + x + '\n');
-                if (x % 10 == 0) continue; /*ничего не делать*/
-                if (x % 2 == 0)
+                log("\n" + DateTime.Now + "\n");
+                Console.Write("Количество Чисел: ");
+                int N = Convert.ToInt32(Console.ReadLine()); /*количество чисел на входе*/
+                log("Введено кол-во чисел: " + N + '\n');
+                int x; /*исходные данные*/
+                int a2 = 0, b2 = 0; /*макс. числа, кратные 2, но не кратные 5*/
+                int a5 = 0, b5 = 0; /*макс. числа, кратные 5, но не кратные 2*/
+                int a0 = 0, b0 = 0; /*максимальные числа, не кратные 5 и 2*/
+                int R; /*введенное контрольное значение*/
+                int m; /*вычисленное контрольное значение*/
+                for (int i = 1; i <= N; ++i)
                 {
-                    if (x > a2) { b2 = a2; a2 = x; }
-                    else if (x > b2) b2 = x;
+                    Console.Write("Исходные Данные: ");
+                    x = Convert.ToInt32(Console.ReadLine());
+                    log("Введено значение для: Исходные Данные " + x + '\n');
+                    if (x % 10 == 0) continue; /*ничего не делать*/
+                    if (x % 2 == 0)
+                    {
+                        if (x > a2) { b2 = a2; a2 = x; }
+                        else if (x > b2) b2 = x;
+                    }
+                    else if (x % 5 == 0)
+                    {
+                        if (x > a5) { b5 = a5; a5 = x; }
+                        else if (x > b5) b5 = x;
+                    }
+                    else
+                    {
+                        if (x > a0) { b0 = a0; a0 = x; }
+                        else if (x > b0) b0 = x;
+                    }
                 }
-                else if (x % 5 == 0)
+                Console.Write("Введите Контрольное Значение(R): ");
+                R = Convert.ToInt32(Console.ReadLine());
+                log("Введенно значение для: Контрольное Значение(R): " + R + '\n');
+                m = a0 * a2;
+                if (a0 * a5 > m) m = a0 * a5;
+                if (a0 * b0 > m) m = a0 * b0;
+                if (a2 * b2 > m) m = a2 * b2;
+                if (a5 * b5 > m) m = a5 * b5;
+                Console.WriteLine("     Получено Чисел: " + N);
+                log("Вывод: Получено Чисел: " + N + '\n');
+                Console.WriteLine("     Принятое Контрольное Значение(R): " + R);
+                log("Вывод: Принятое Контрольное Значение(R): " + R + '\n');
+                if (m > 0) Console.WriteLine("     Вычисленное Контрольное Значение: " + m);
+                log("Выполнен расчет: Вычисленное Контрольное Значение - " + m + '\n');
+                log("Сравнение результатов: Принятое Контрольное Значение и Вычисленное Контрольное Значение." + '\n');
+                if (R > 0 && R == m)
                 {
-                    if (x > a5) { b5 = a5; a5 = x; }
-                    else if (x > b5) b5 = x;
+                    Console.WriteLine("     Контрольное Значение Соответствует Введеному.");
+                    log("Вывод: Вывод в консоль соответствия" + '\n');
                 }
                 else
                 {
-                    if (x > a0) { b0 = a0; a0 = x; }
-                    else if (x > b0) b0 = x;
+                    Console.WriteLine("     Контрольное Значение НЕ Соответствует.");
+                    log("Вывод: Вывод в консоль НЕ соответствия" + '\n');
                 }
             }
-            Console.Write("Введите Контрольное Значение(R): ");
-            R = Convert.ToInt32(Console.ReadLine());
-            log("Введенно значение для: Контрольное Значение(R): " + R + '\n');
-            m = a0 * a2;
-            if (a0 * a5 > m) m = a0 * a5;
-            if (a0 * b0 > m) m = a0 * b0;
-            if (a2 * b2 > m) m = a2 * b2;
-            if (a5 * b5 > m) m = a5 * b5;
-            Console.WriteLine("     Получено Чисел: " + N);
-            log("Вывод: Получено Чисел: " + N + '\n');
-            Console.WriteLine("     Принятое Контрольное Значение(R): "+ R );
-            log("Вывод: Принятое Контрольное Значение(R): " + R + '\n');
-            if (m > 0) Console.WriteLine("     Вычисленное Контрольное Значение: "+ m); 
-            log("Выполнен расчет: Вычисленное Контрольное Значение - " + m + '\n');
-            log("Сравнение результатов: Принятое Контрольное Значение и Вычисленное Контрольное Значение." + '\n');
-            if (R > 0 && R == m)
-            {
-                Console.WriteLine("     Контрольное Значение Соответствует Введеному.");
-                log("Вывод: Вывод в консоль соответствия" + '\n');
-            }
-            else
-            {
-                Console.WriteLine("     Контрольное Значение НЕ Соответствует.");
-                log("Вывод: Вывод в консоль НЕ соответствия" + '\n');
-            }
+            catch(Exception)
+            { Console.WriteLine("Error!"); }
         }
     }
 }
